5b7ef9036d4924b1c6a1b290be72ffc4ebdde291
Samarth fere <samarthfere6@gmail.com>
2026-01-03 23:44:52 +0530

    Add comments to explain array reversal logic
    Added comments to explain the pointers and swapping logic.

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -1,7 +1,9 @@
 public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-
+// start points to the first index
+// end points to the last index
+// swap elements until both pointers meet
         int start = 0;
         int end = arr.length - 1;
 
